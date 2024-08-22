250c506caa0df91ed4a965d1cbdc9886faf85768
Abdulsomod Babalola <samadbabalola09@gmail.com>
2024-08-22 15:05:30 +0100

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import (BlogListView,BlogCreateView,BlogDetailView,BlogUpdateView,BlogDeleteView,profileBlogListView )
+
+urlpatterns = [
+    path("post/<int:pk>/delete/",BlogDeleteView.as_view(),name='post_delete'),
+  path("post/<int:pk>/edit/",BlogUpdateView.as_view(), name="post_edit"),
+  path("post/new/", BlogCreateView.as_view(), name="new_post"),
+  path("post/<int:pk>/",BlogDetailView.as_view(),name="post_detail"),
+  path('', BlogListView.as_view(), name='home'),
+  path('profile/', profileBlogListView.as_view(), name='profile')
+]
